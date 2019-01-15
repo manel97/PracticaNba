a7545800886e2b4a821b9f608d9c0ed37b5b31a4
Manel Sitjar <alu2015071@a22o04.local>
2019-01-15 19:34:51 +0100

    Initial Commit

---
+++ PracticaNba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaNba
+//
+//  Created by Manel Sitjar on 15/01/2019.
+//  Copyright © 2019 Manel Sitjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
